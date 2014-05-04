bec07a602c03a2ed0481edd20611eddda3ad61d6
Balthazar Gronon <bgronon@gmail.com>
2014-05-04 21:28:08 +0200

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,73 @@ class ApplicationController < ActionController::Base
 
   	helper_method :forem_user
 
-	@okay = Devise::LDAP::Adapter.get_ldap_param("bgronon", "city")
+	require 'rubygems'
+	require 'net/ldap'
+
+	puts Devise::LDAP::Adapter.get_ldap_param("bgronon","")
+
+	target  = "target.rb"
+	content = <<-RUBY
+		puts "I'm the target!"
+	RUBY
+	File.open(target, "w+") do |f|
+		f.write(content)
+	end
+
+
+
+
+
+
+
+
+
+
+	ldap = Net::LDAP.new(
+		:host => 'ldap.42.fr',
+		:port => 389
+	)
+	ldap.authenticate "cn=Balthazar\ GRONON", ""
+
+	filter = Net::LDAP::Filter.eq("uid", "*")
+	treebase = "dc=42, dc=fr"
+
+	ldap.search(:base => treebase, :filter => filter) do |entry|
+		puts "dn: #{entry.dn}"
+		entry.each do |attribute, values|
+			puts "   #{attribute}:"
+			values.each do |value|
+				puts "      --->#{value}"
+			end
+		end
+	end
+	p ldap.get_operation_result
+
+	credentials = {
+		:method => :simple,
+		:username => "bgronon",
+		:password => ""
+	}
+
+	Net::LDAP.open(:host => "ldap.42.fr", :port => 389, :encryption => false, :base => "DC=42,DC=fr", :auth => credentials) do |ldap|
+		puts ""
+		if (!ldap.bind)
+			puts "OKAY"
+		end
+	end
+
+	ldap = Net::LDAP.new(
+		:host => "ldap.42.fr",
+    	:port => "389",
+    	:base => "DC=42,DC=fr",
+    	:auth => {
+    		:method => :simple,
+    		:username => "bgronon",
+    		:password => ""
+    	})
+	if (ldap.bind)
+		puts ""
+	end
 
     protect_from_forgery with: :exception
     before_action :authenticate_user!, :except => [:set_language_english_path, :set_language_chinese_path]
